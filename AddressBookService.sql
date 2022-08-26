29b58f2b2f1a54f0384a8fa0d446de2c201dae00
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-26 21:53:16 +0530

    created addressbook table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -3,3 +3,17 @@
 create database AddressBookService
 
 use AddressBookService
+
+-----UC-2------
+
+create table AddressBook
+(
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Address varchar(50) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zipcode varchar(50) not null,
+PhoneNumber varchar(50) not null,
+Email varchar(50) not null
+)
